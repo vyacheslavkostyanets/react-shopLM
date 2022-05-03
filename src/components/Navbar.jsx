275c160d6d29f8ea288687959a592a5a0940dd7d
Vichaslav Kostianets <vyacheslav.kostyanets@gmail.com>
2022-05-03 22:17:57 +0300

    make a slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Container = styled.div`
-  height: 60px;
+  height: auto;
 `;
 
 const Wrapper = styled.div`
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+import banner from "../assets/images/banner.jpg";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 80vh;
   display: flex;
-  background-color: coral;
+  /* background-color: coral; */
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -28,12 +30,82 @@ const Arrow = styled.div`
   opacity: 0.4;
 `;
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 2;
+`;
+const Image = styled.img`
+  height: 80%;
+  width: 100%;
+`;
+const InfoContainer = styled.div`
+  padding: 40px;
+  flex: 1;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
         <ArrowLeftOutlinedIcon />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src={banner} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Summer sale</Title>
+            <Desc>DON`T COMPROMISE</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImgContainer>
+            <Image src={banner} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Winter sale</Title>
+            <Desc>DON`T COMPROMISE</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide>
+          <ImgContainer>
+            <Image src={banner} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>50% sale</Title>
+            <Desc>DON`T COMPROMISE</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlinedIcon />
       </Arrow>
