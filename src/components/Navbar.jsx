a5c13b5e039cddcd4b84f224b683c56e5a7734f5
Vichaslav Kostianets <vyacheslav.kostyanets@gmail.com>
2022-05-03 20:04:34 +0300

    added material-ua and styled header block

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,51 @@
+import { Search } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightblue;
 `;
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>En</Language>
+          <SearchContainer>
+            Input
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
